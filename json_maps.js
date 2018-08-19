7e90d586b61d9e83095a1c68a9a185425dc7a640
arseniykhmelidze <arseniykhmelidze@ucsc.edu>
2018-08-19 16:35:50 +0300

    Added Initial Files

---
+++ json_maps.js
@@ -0,0 +1,26 @@
+    var map = new Object();
+    var i;
+    var location_name;
+
+    function initMap() // Initializing map
+        {    
+           map = new google.maps.Map(document.getElementById('map_area'), 
+            {
+                center: {lat: 55.752023, lng: 37.617499}, // Moscow Kremlin
+                zoom: 10
+             });
+            
+            map.data.loadGeoJson('moscow_locations.json'); // Loading JSON Layers
+            
+                    
+            var infowindow = new google.maps.InfoWindow(
+            {
+                content: ""
+            });
+            
+    //////////////// CURSOR ////////////////////////////////////////////////////////////////////////////////////
+     
+            map.setOptions({ draggableCursor: 'default' });  // custom cursor
+
+            
+        } // End of initMap
